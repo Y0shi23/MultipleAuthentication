012216a6e754dcb79820fd9334e2cb1e8c557978
長田好史 <yoshifumi042@gmail.com>
2019-09-03 23:00:04 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::group(['middleware' => 'auth:user'], function() {
 */
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/',         function () { return redirect('/admin/home'); });
-    Route::get('login',     'Admin\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login',    'Admin\LoginController@login');
+    Route::get('/login',     'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login',    'Admin\LoginController@login');
 });
 
 /*
@@ -43,6 +43,6 @@ Route::group(['prefix' => 'admin'], function() {
 |--------------------------------------------------------------------------
 */
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
-    Route::post('logout',   'Admin\LoginController@logout')->name('admin.logout');
-    Route::get('home',      'Admin\HomeController@index')->name('admin.home');
+    Route::post('/logout',   'Admin\LoginController@logout')->name('admin.logout');
+    Route::get('/home',      'Admin\HomeController@index')->name('admin.auth.home');
 });
